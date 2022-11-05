527f1e59cb8a8947bf14e80bf008b3186b808a63
Samuel Olugbesan <samuelolugbesan@gmail.com>
2022-11-05 20:03:42 +0100

    The first time

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn dentist.wsgi:application --log-file -
---
+++ website/views.py
@@ -0,0 +1,40 @@
+from turtle import setundobuffer
+from django.shortcuts import render
+from django.core.mail import send_mail
+from dentist import settings
+
+# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+
+        form_data ={
+        'message_name': message_name,
+        'message_email': message_email,
+        'message': message,
+        }
+        message = '''
+        From:\n\t\t{}\n
+        Message:\n\t\t{}\n
+        Email:\n\t\t{}\n
+        '''.format(form_data['message_name'], form_data['message'], form_data['message_email'])
+
+        
+        
+        send_mail(
+            'You got a mail',
+            message,
+            '',
+            ['essteeooh@gmail.com'],
+            fail_silently=False
+        )
+
+        return render(request, 'index.html', {'message_name': message_name})
+    
+    else:
+       return render(request, 'index.html', {})
+
